766ffb6e855476bd40e514a32f11af9e64befecb
Andrew Zabelin <letzabelin@gmail.com>
2023-03-28 16:52:30 +0300

    feat: done TupleToObject type

--- src/easy/pick.ts
+++ src/easy/pick.ts
@@ -1,3 +1,6 @@
+// Implement the built-in Pick<T, K> generic without using it.
+// Constructs a type by picking the set of properties K from T
+
 type MyPick<T, K extends keyof T> = {
   [key in K]: T[key];
 };
--- src/easy/readonly.ts
+++ src/easy/readonly.ts
@@ -1,3 +1,6 @@
+// Implement the built-in Readonly<T> generic without using it.
+// Constructs a type with all properties of T set to readonly, meaning the properties of the constructed type cannot be reassigned.
+
 type MyReadonly<T> = {
   readonly [key in keyof T]: T[key];
 };
