6ed368eb49ab23329ae177c78d4b318174bb1d34
Andrew Zabelin <letzabelin@gmail.com>
2023-03-30 15:10:00 +0300

    feat: done Concat type

---
+++ src/easy/concat.ts
@@ -0,0 +1,6 @@
+// https://github.com/type-challenges/type-challenges/blob/main/questions/00533-easy-concat/README.md
+
+// eslint-disable-next-line
+type Concat<T extends any[], U extends any[]> = [...T, ...U]
+
+export {};
