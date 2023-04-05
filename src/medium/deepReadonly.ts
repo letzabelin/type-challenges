901a788593c91728dfe69449b426a1e8d4d301b2
Andrew Zabelin <letzabelin@gmail.com>
2023-04-05 12:30:43 +0300

    chore: add another DeepReadonly solution

--- src/medium/deepReadonly.ts
+++ src/medium/deepReadonly.ts
@@ -1,9 +1,17 @@
 // https://github.com/type-challenges/type-challenges/blob/main/questions/00009-medium-deep-readonly/README.md
 
-export type DeepReadonly<T> = {
-  readonly [key in keyof T]: T[key] extends Record<string, unknown>
-    ? DeepReadonly<T[key]>
-    : T[key] extends [infer First, ...infer Rest]
-      ? readonly [DeepReadonly<First>, ...DeepReadonly<Rest>]
-      : T[key];
-};
+export type DeepReadonly<T> = T extends (...args: unknown[]) => unknown
+  ? T
+  : T extends unknown
+    ? {
+      readonly [key in keyof T]: DeepReadonly<T[key]>;
+    }
+    : T;
+
+// export type DeepReadonly<T> = {
+//   readonly [key in keyof T]: T[key] extends Record<string, unknown>
+//     ? DeepReadonly<T[key]>
+//     : T[key] extends [infer First, ...infer Rest]
+//       ? readonly [DeepReadonly<First>, ...DeepReadonly<Rest>]
+//       : T[key];
+// };
